2ba252783a262ab160e421afc76758f51cf0ce94
Jean-Baptiste Camps <jbcamps@hotmail.com>
2019-07-06 16:25:45 +0200

    txt and lang classif

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import sys
+import os
 import preproc.tuyau as tuy
 import fasttext
-#from importlib import reload
-#tuy = reload(tuy)
+# from importlib import reload
+# tuy = reload(tuy)
+#import json , json.dump, file et object, json.load sur des files, dumps et loads sur des str
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
 
-    #path = "meertens-song-collection-DH2019/train/34153.xml"
+    # path = "meertens-song-collection-DH2019/train/34153.xml"
     myTexts = []
 
-    lowCerts = []
+    langCerts = []
 
     model = fasttext.load_model("preproc/models/lid.176.bin")
 
@@ -19,15 +21,24 @@ if __name__ = '__main__':
             name = path.split('/')[-1]
             aut, text = tuy.XML_to_text(path)
             lang, cert = tuy.identify_lang(text, model)
+            lang = lang[0].replace("__label__", "")
 
-            myTexts.append({"name": name, "aut": aut, "text": text, "lang:" lang})
+            myTexts.append({"name": name, "aut": aut, "text": text, "lang": lang})
 
-            if cert < 0.9:
-                lowCerts.append((lang, name, cert))
+            #if cert < 1:
+            langCerts.append((lang, name, cert))
 
-            with open("txt/" + lang + "/" + aut + "/" + name + ".txt", "w") as out:
+            directory = "txt/" + lang + "/" + aut + "/"
+
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+
+            with open(directory + name + ".txt", "w") as out:
                 out.write(text)
 
+    with open("lang_certs.csv", 'w') as out:
+        for line in langCerts:
+            out.write("{}\t{}\t{}\t\n".format(line[0], line[1], float(line[2])))
 
 
 
