27648ea3f684d0e2cb3327289da6992b54f29c14
Jean-Baptiste Camps <jbcamps@hotmail.com>
2019-07-06 18:33:34 +0200

    feats in Thibault's order

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ import fasttext
 #import json
 import nltk.tokenize
 
+# TODO: eliminate features that occur only n times ?
+# Do the Moisl Selection ?
+# Z-scores, etc. ?
+# Vector-length normalisation ?
+#
 
 if __name__ == '__main__':
 
@@ -27,7 +32,7 @@ if __name__ == '__main__':
             lang = lang[0].replace("__label__", "")
 
             myTexts.append({"name": name, "aut": aut, "text": text, "lang": lang,
-                            "wordCounts": fex.count_words(text, relFreqs=False)})
+                            "wordCounts": fex.count_words(text.lower(), relFreqs=True)})
 
             #if cert < 1:
             #langCerts.append((lang, name, cert))
@@ -47,15 +52,17 @@ if __name__ == '__main__':
     unique_words = set([k for t in myTexts for k in t["wordCounts"].keys()])
 
     with open("feats.csv", "w") as out:
-        for text in myTexts:
-            out.write("\t"+text["name"])
+        # First line
+        out.write("\t" + "author" + "\t" + "lang")
+        for word in unique_words:
+            out.write("\t"+word)
 
         out.write("\n")
 
-        for word in unique_words:
-            out.write(word)
-            for text in myTexts:
-                if not word in text["wordCounts"]:
+        for text in myTexts:
+            out.write(text["name"] + "\t" + text["aut"] + "\t" + text["lang"])
+            for word in unique_words:
+                if not word in text["wordCounts"].keys():
                     out.write("\t"+"0")
 
                 else:
