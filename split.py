c28995358229b9f8120b2ec141b97b0fd53394e7
Jean-Baptiste Camps <jbcamps@hotmail.com>
2019-07-08 19:25:52 +0200

    selector

--- split.py
+++ split.py
@@ -1,9 +1,13 @@
-
-
+import jagen_will.preproc.select as sel
 
 if __name__ == '__main__':
-    path = "feats_tests.csv"
-    path.split(".")[0]
-    'feats_test'
-    metadata_path = "langcert_revised.csv"
-    excludes_path = "wilhelmus_train.csv"
+
+    # to create initial selection
+    sel.read_clean_split(path="feats_tests.csv",
+                         metadata_path="langcert_revised.csv",
+                         excludes_path="wilhelmus_train.csv",
+                         savesplit="split.json"
+                         )
+
+    # to load and apply a selection
+    sel.apply_selection(path="feats_tests.csv", presplit_path='split.json')
