4b65e18b6d52269778d5327efa15f32e5dc05dde
Jean-Baptiste Camps <jbcamps@hotmail.com>
2021-02-04 20:41:21 +0100

    try to load txt

--- jagen_will/preproc/tuyau.py
+++ jagen_will/preproc/tuyau.py
@@ -51,6 +51,24 @@ def XML_to_text(path, correct_aut=None):
 
         return aut, re.sub(r"\s+", " ", str(myxsl(my_doc)))
 
+def TXT_to_text(path, correct_aut=None):
+    """
+    Get main text from xml file
+    :param path: path to the file to transform
+    :param correct_aut: optional data frame of metadata correction (authors)
+    :return: a tuple with auts, and string (the text).
+    """
+
+    with open(path, 'r') as f:
+        txt = f.readlines()
+
+    # get author from filename (string before first _)
+    aut = path.split('/')[-1].split("_")[0]
+
+    return aut, re.sub(r"\s+", " ", str(txt))
+
+
+
 
 def identify_lang(string, model):
     """
@@ -70,7 +88,7 @@ def normalise(text):
     return unidecode.unidecode(re.sub(r"\s+", " ", re.sub(r"[\W0-9]+", " ", text.lower()).strip()))
 
 
-def load_texts(paths, fasttext_model, correct_aut=None):
+def load_texts(paths, fasttext_model, format=txt, correct_aut=None):
     """
     Loads a collection of documents into a 'myTexts' object for further processing.
     TODO: a proper class
@@ -85,7 +103,13 @@ def load_texts(paths, fasttext_model, correct_aut=None):
 
     for path in paths:
         name = path.split('/')[-1]
-        aut, text = XML_to_text(path, correct_aut=correct_aut)
+
+        if format=='xml':
+            aut, text = XML_to_text(path, correct_aut=correct_aut)
+
+        else:
+            aut, text = TXT_to_text(path) #implement correct_aut
+
         lang, cert = identify_lang(text, fasttext_model)
         lang = lang[0].replace("__label__", "")
 
