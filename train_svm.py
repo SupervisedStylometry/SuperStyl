c39dfca7d32fcd0fbe2e99d54dfe377030436449
Jean-Baptiste Camps <jbcamps@hotmail.com>
2021-02-06 13:40:35 +0100

    Implementing leave one out and sk pipelines

--- train_svm.py
+++ train_svm.py
@@ -8,7 +8,8 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('train_path', action='store', help="Path to train file", type=str)
-    parser.add_argument('test_path', action='store', help="Path to test file", type=str)
+    parser.add_argument('--test_path', action='store', help="Path to test file", type=str, required=False, default=None)
+    parser.add_argument('--leave_one_out', action='store', help="perform leave-one-out cross validation (test_path will be used only for final prediction)", default=False)
     parser.add_argument('--dim_reduc', action='store', choices=['pca', 'som'], help="optional dimensionality reduction of input data", default=None)
     parser.add_argument('--norms', action='store_true', help="perform normalisations?", default=False)
     parser.add_argument('--kernel', action='store',
@@ -22,9 +23,14 @@ if __name__ == "__main__":
     # test_path = "data/feats_tests_valid_realForTest.csv"
     print(".......... loading data ........")
     train = pandas.read_csv(args.train_path, index_col=0)
-    test = pandas.read_csv(args.test_path, index_col=0)
 
-    svm = jagen_will.svm.train_svm(train, test, dim_reduc=args.dim_reduc, norms=args.norms,
+    if args.test_path is not None:
+        test = pandas.read_csv(args.test_path, index_col=0)
+
+    else:
+        test = None
+
+    svm = jagen_will.svm.train_svm(train, test, leave_one_out=args.leave_one_out, dim_reduc=args.dim_reduc, norms=args.norms,
                                    kernel=args.kernel, final_pred=args.final)
 
     joblib.dump(svm, 'mySVM.joblib')
