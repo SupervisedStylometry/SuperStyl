a2b9a246748eeb7f2a4b36ae80bd44b7e1aa1878
Jean-Baptiste Camps <jbcamps@hotmail.com>
2024-02-20 18:48:25 +0100

    moving on tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -116,6 +116,137 @@ class Main(unittest.TestCase):
 
     # TODO: test other loading formats with sampling, that are not txt (and decide on their implementation)
 
+    # Testing the processing of "myTexts" objects
+    def test_get_feature_list(self):
+        # FEATURE For a myTexts object with several texts, extract the relevant features (words or chars n-grams)
+        # GIVEN
+        myTexts = [
+            {"name": "Letter1", "aut": "Smith", "text": "This is the text", "lang": "en"},
+            {"name": "Letter2", "aut": "Smith", "text": "This is also the text", "lang": "en"},
+            {"name": "Letter1", "aut": "Dupont", "text": "Voici le texte", "lang": "fr"},
+        ]
+        # WHEN
+        results = superstyl.preproc.features_extract.get_feature_list(myTexts, feats="words", n=1, relFreqs=False)
+        # THEN
+        expected = [('This', 2), ('is', 2), ('the', 2), ('text', 2), ('also', 1), ('Voici', 1), ('le', 1), ('texte', 1)]
+        self.assertEqual(results, expected)
+
+        # WHEN
+        results = superstyl.preproc.features_extract.get_feature_list(myTexts, feats="words", n=1, relFreqs=True)
+        # THEN
+        expected = [('This', 2/12), ('is', 2/12), ('the', 2/12), ('text', 2/12),  ('also', 1/12), ('Voici', 1/12),
+                    ('le', 1/12), ('texte', 1/12)]
+        self.assertEqual(results, expected)
+
+        # WHEN
+        results = superstyl.preproc.features_extract.get_feature_list(myTexts, feats="words", n=2, relFreqs=False)
+
+        # THEN
+        expected = [('This_is', 2), ('the_text', 2), ('is_the', 1), ('is_also', 1), ('also_the', 1), ('Voici_le', 1), ('le_texte', 1)]
+        self.assertEqual(results, expected)
+
+        # WHEN
+        results = superstyl.preproc.features_extract.get_feature_list(myTexts[0:2], feats="chars", n=2, relFreqs=False)
+
+        # THEN
+        expected = [('is', 4), ('s_', 4), ('_t', 4), ('Th', 2), ('hi', 2), ('_i', 2), ('th', 2), ('he', 2), ('e_', 2),
+                    ('te', 2), ('ex', 2), ('xt', 2), ('_a', 1), ('al', 1), ('ls', 1), ('so', 1), ('o_', 1)]
+        self.assertEqual(results, expected)
+
+        # WHEN
+        results = superstyl.preproc.features_extract.get_feature_list(myTexts[0:2], feats="chars", n=2, relFreqs=True)
+
+        # THEN
+        expected = [('is', 4/35), ('s_', 4/35), ('_t', 4/35), ('Th', 2/35), ('hi', 2/35), ('_i', 2/35), ('th', 2/35),
+                    ('he', 2/35), ('e_', 2/35), ('te', 2/35), ('ex', 2/35), ('xt', 2/35), ('_a', 1/35), ('al', 1/35),
+                    ('ls', 1/35), ('so', 1/35), ('o_', 1/35)]
+        self.assertEqual(results, expected)
+
+
+    def test_get_counts(self):
+        # SCENARIO: given a myTexts object, i.e. a list of dictionaryies, containing metadata and text, count
+        # the frequencies of features inside
+        myTexts = [
+            {"name": "Letter1", "aut": "Smith", "text": "This is the text", "lang": "en"},
+            {"name": "Letter2", "aut": "Smith", "text": "This is also the text", "lang": "en"},
+            {"name": "Letter1", "aut": "Dupont", "text": "Voici le texte", "lang": "fr"},
+        ]
+        # WHEN
+        results = superstyl.preproc.features_extract.get_counts(myTexts, ['the', 'is', 'also', 'le'], feats = "words",
+                                                          n = 1, relFreqs = True)
+        # THEN
+        expected = [{'name': 'Letter1', 'aut': 'Smith', 'text': 'This is the text', 'lang': 'en',
+              'wordCounts': {'the': 0.25, 'is': 0.25}},
+             {'name': 'Letter2', 'aut': 'Smith', 'text': 'This is also the text', 'lang': 'en',
+              'wordCounts': {'the': 0.2, 'is': 0.2, 'also': 0.2}},
+             {'name': 'Letter1', 'aut': 'Dupont', 'text': 'Voici le texte', 'lang': 'fr', 'wordCounts': {'le': 1/3}}]
+        self.assertEqual(results, expected)
+
+        # WHEN
+        results = superstyl.preproc.features_extract.get_counts(myTexts, feats="words", n=1, relFreqs=True)
+        # THEN
+        expected = [{'name': 'Letter1', 'aut': 'Smith', 'text': 'This is the text', 'lang': 'en',
+                     'wordCounts': {'This': 0.25, 'is': 0.25, 'the': 0.25, 'text': 0.25}},
+                   {'name': 'Letter2', 'aut': 'Smith', 'text': 'This is also the text', 'lang': 'en', 'wordCounts':
+                       {'This': 0.2, 'is': 0.2, 'also': 0.2, 'the': 0.2, 'text': 0.2}},
+                    {'name': 'Letter1', 'aut': 'Dupont', 'text': 'Voici le texte', 'lang': 'fr', 'wordCounts':
+                        {'Voici': 1/3, 'le': 1/3, 'texte': 1/3}}]
+        self.assertEqual(results, expected)
+
+        # WHEN
+        results = superstyl.preproc.features_extract.get_counts(myTexts, feats="words", n=2, relFreqs=False)
+        # THEN
+        expected = [{'name': 'Letter1', 'aut': 'Smith', 'text': 'This is the text', 'lang': 'en', 'wordCounts':
+            {'This_is': 1, 'is_the': 1, 'the_text': 1}},
+                    {'name': 'Letter2', 'aut': 'Smith', 'text': 'This is also the text', 'lang': 'en', 'wordCounts':
+                        {'This_is': 1, 'is_also': 1, 'also_the': 1, 'the_text': 1}},
+                    {'name': 'Letter1', 'aut': 'Dupont', 'text': 'Voici le texte', 'lang': 'fr', 'wordCounts':
+                        {'Voici_le': 1, 'le_texte': 1}}]
+        self.assertEqual(results, expected)
+
+        # WHEN
+        results = superstyl.preproc.features_extract.get_counts(myTexts, feats="words", n=2, relFreqs=True)
+        # THEN
+        expected = [{'name': 'Letter1', 'aut': 'Smith', 'text': 'This is the text', 'lang': 'en', 'wordCounts':
+            {'This_is': 1/3, 'is_the': 1/3, 'the_text': 1/3}},
+                    {'name': 'Letter2', 'aut': 'Smith', 'text': 'This is also the text', 'lang': 'en', 'wordCounts':
+                        {'This_is': 1/4, 'is_also': 1/4, 'also_the': 1/4, 'the_text': 1/4}},
+                    {'name': 'Letter1', 'aut': 'Dupont', 'text': 'Voici le texte', 'lang': 'fr', 'wordCounts':
+                        {'Voici_le': 1/2, 'le_texte': 1/2}}]
+        self.assertEqual(results, expected)
+
+        # WHEN
+        results = superstyl.preproc.features_extract.get_counts(myTexts, feat_list=["This_is", "le_texte"],
+                                                                feats="words", n=2, relFreqs=True)
+        # THEN
+        expected = [{'name': 'Letter1', 'aut': 'Smith', 'text': 'This is the text', 'lang': 'en', 'wordCounts':
+            {'This_is': 1 / 3}},
+                    {'name': 'Letter2', 'aut': 'Smith', 'text': 'This is also the text', 'lang': 'en', 'wordCounts':
+                        {'This_is': 1/4}},
+                    {'name': 'Letter1', 'aut': 'Dupont', 'text': 'Voici le texte', 'lang': 'fr', 'wordCounts':
+                        {'le_texte': 1 / 2}}]
+        self.assertEqual(results, expected)
+
+        # WHEN
+        results = superstyl.preproc.features_extract.get_counts(myTexts, feat_list=["th"],
+                                                                feats="chars", n=2, relFreqs=True)
+        # THEN
+        expected = [{'name': 'Letter1', 'aut': 'Smith', 'text': 'This is the text', 'lang': 'en',
+                     'wordCounts': {'th': 1/15}},
+                    {'name': 'Letter2', 'aut': 'Smith', 'text': 'This is also the text', 'lang': 'en',
+                     'wordCounts': {'th': 1/20}},
+                    {'name': 'Letter1', 'aut': 'Dupont', 'text': 'Voici le texte', 'lang': 'fr', 'wordCounts': {}}]
+
+        self.assertEqual(results, expected)
+
+    # TODO: test get_embedded_counts and load_embedding
+
+    # TODO: test count_process
+
+    # TODO: test features_select
+    # TODO: test select
+
+
 class DataLoading(unittest.TestCase):
 
      # Now down to lower level features
@@ -180,66 +311,6 @@ class DataLoading(unittest.TestCase):
         results = superstyl.preproc.tuyau.max_sampling(myTexts, max_samples=1)
         # EXPECT
         self.assertEqual(len([text for text in results if text["aut"] == 'Smith']), 1)
-
-    # Testing the processing of "myTexts" objects
-    def test_get_feature_list(self):
-        # FEATURE For a myTexts object with several texts, extract the relevant features (words or chars n-grams)
-        # GIVEN
-        myTexts = [
-            {"name": "Letter1", "aut": "Smith", "text": "This is the text", "lang": "en"},
-            {"name": "Letter2", "aut": "Smith", "text": "This is also the text", "lang": "en"},
-            {"name": "Letter1", "aut": "Dupont", "text": "Voici le texte", "lang": "fr"},
-        ]
-        # WHEN
-        results = superstyl.preproc.features_extract.get_feature_list(myTexts, feats="words", n=1, relFreqs=False)
-        # THEN
-        expected = [('This', 2), ('is', 2), ('the', 2), ('text', 2), ('also', 1), ('Voici', 1), ('le', 1), ('texte', 1)]
-        self.assertEqual(results, expected)
-
-        # WHEN
-        results = superstyl.preproc.features_extract.get_feature_list(myTexts, feats="words", n=1, relFreqs=True)
-        # THEN
-        # TODO: BUG (sum of relative frequencies?)
-        expected = [('This', 2/12), ('is', 2/12), ('the', 2/12), ('text', 2/12),  ('also', 1/12), ('Voici', 1/12),
-                    ('le', 1/12), ('texte', 1/12)]
-        self.assertEqual(results, expected)
-
-        # WHEN
-        results = superstyl.preproc.features_extract.get_feature_list(myTexts, feats="words", n=2, relFreqs=False)
-
-        # THEN
-        expected = [('This_is', 2), ('the_text', 2), ('is_the', 1), ('is_also', 1), ('also_the', 1), ('Voici_le', 1), ('le_texte', 1)]
-        self.assertEqual(results, expected)
-
-        # WHEN
-        results = superstyl.preproc.features_extract.get_feature_list(myTexts[0:2], feats="chars", n=2, relFreqs=False)
-
-        # THEN
-        expected = [('is', 4), ('s_', 4), ('_t', 4), ('Th', 2), ('hi', 2), ('_i', 2), ('th', 2), ('he', 2), ('e_', 2),
-                    ('te', 2), ('ex', 2), ('xt', 2), ('_a', 1), ('al', 1), ('ls', 1), ('so', 1), ('o_', 1)]
-        self.assertEqual(results, expected)
-
-
-
-
-    def test_get_counts(self):
-        myTexts = [
-            {"name": "Letter1", "aut": "Smith", "text": "This is the text", "lang": "en"},
-            {"name": "Letter2", "aut": "Smith", "text": "This is also the text", "lang": "en"},
-            {"name": "Letter1", "aut": "Dupont", "text": "Voici le texte", "lang": "fr"},
-        ]
-
-        self.assertEqual(
-            superstyl.preproc.features_extract.get_counts(myTexts, ['the', 'is', 'also', 'le'], feats = "words",
-                                                          n = 1, relFreqs = True),
-            [{'name': 'Letter1', 'aut': 'Smith', 'text': 'This is the text', 'lang': 'en',
-              'wordCounts': {'the': 0.25, 'is': 0.25}},
-             {'name': 'Letter2', 'aut': 'Smith', 'text': 'This is also the text', 'lang': 'en',
-              'wordCounts': {'the': 0.2, 'is': 0.2, 'also': 0.2}},
-             {'name': 'Letter1', 'aut': 'Dupont', 'text': 'Voici le texte', 'lang': 'fr', 'wordCounts': {'le': 1/3}}]
-        )
-
-        #TODO: a lot more tests
 
 
 # TODO: tests for SVM, etc.
