5c191f93477aa43ab2c36a77266192450e0b0f5f
Jean-Baptiste Camps <jbcamps@hotmail.com>
2024-02-27 13:08:12 +0100

    no with minimal testing

---
+++ tests/test_train_svm.py
@@ -0,0 +1,30 @@
+import unittest
+import superstyl
+import os
+import pandas
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+class Main_svm(unittest.TestCase):
+    def test_train_svm(self):
+        # SCENARIO: train a svm and test it
+        # GIVEN
+        train = pandas.DataFrame({'author': {'Dupont_Letter1.txt': 'Dupont', 'Smith_Letter1.txt': 'Smith', 'Smith_Letter2.txt': 'Smith'},
+                           'lang': {'Dupont_Letter1.txt': 'NA', 'Smith_Letter1.txt': 'NA', 'Smith_Letter2.txt': 'NA'},
+                           'this': {'Dupont_Letter1.txt': 0.0, 'Smith_Letter1.txt': 0.25, 'Smith_Letter2.txt': 0.2},
+                           'is': {'Dupont_Letter1.txt': 0.0, 'Smith_Letter1.txt': 0.25, 'Smith_Letter2.txt': 0.2},
+                           'the': {'Dupont_Letter1.txt': 0.0, 'Smith_Letter1.txt': 0.25, 'Smith_Letter2.txt': 0.2},
+                           'text': {'Dupont_Letter1.txt': 0.0, 'Smith_Letter1.txt': 0.25, 'Smith_Letter2.txt': 0.2},
+                           'voici': {'Dupont_Letter1.txt': 1/3, 'Smith_Letter1.txt': 0.0, 'Smith_Letter2.txt': 0.0},
+                           'le': {'Dupont_Letter1.txt': 1/3, 'Smith_Letter1.txt': 0.0, 'Smith_Letter2.txt': 0.0},
+                           'texte': {'Dupont_Letter1.txt': 1/3, 'Smith_Letter1.txt': 0.0, 'Smith_Letter2.txt': 0.0},
+                           'also': {'Dupont_Letter1.txt': 0.0, 'Smith_Letter1.txt': 0.0, 'Smith_Letter2.txt': 0.2}})
+        test = train # easier to make no mistake :)
+        # WHEN
+        results = superstyl.train_svm(train,test, final_pred=True)
+        # THEN
+        expected_results = {'filename': {0: 'Dupont_Letter1.txt', 1: 'Smith_Letter1.txt', 2: 'Smith_Letter2.txt'},
+                            'author': {0: 'Dupont', 1: 'Smith', 2: 'Smith'},
+                            'Decision function': {0: -0.7840855202465706, 1: 0.869169888531951, 2: 0.8619190699737995}}
+        self.assertEqual(results["final_predictions"].to_dict()['author'], expected_results['author'])
+        # This is only the first minimal test for this function
--- train_svm.py
+++ train_svm.py
@@ -2,47 +2,55 @@ import superstyl.svm
 import pandas
 import joblib
 
-
 if __name__ == "__main__":
 
     import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument('train_path', action='store', help="Path to train file", type=str)
-    parser.add_argument('--test_path', action='store', help="Path to test file", type=str, required=False, default=None)
+    parser.add_argument('--test_path', action='store', help="Path to test file", type=str, required=False,
+                        default=None)
+    parser.add_argument('-o', action='store', help="optional prefix for output files", type=str,
+                        default=None)
     parser.add_argument('--cross_validate', action='store',
                         help="perform cross validation (test_path will be used only for final prediction)."
                              "If group_k-fold is chosen, each source file will be considered a group "
                              "(only relevant if sampling was performed and more than one file per class was provided)",
                         default=None, choices=['leave-one-out', 'k-fold', 'group-k-fold'], type=str)
     parser.add_argument('--k', action='store', help="k for k-fold", default=10, type=int)
-    parser.add_argument('--dim_reduc', action='store', choices=['pca'], help="optional dimensionality reduction of input data (warn: som is broken)", default=None)
-    parser.add_argument('--norms', action='store_true', help="perform normalisations? (default: True)", default=True)
-    parser.add_argument('--balance', action='store', choices=["downsampling", "Tomek", "upsampling", "SMOTE", "SMOTETomek"],
-        help="which "
-            "strategy to use in case of imbalanced datasets: "
-            "downsampling (random without replacement), "
-            "Tomek (downs. by removing Tomek links), "
-#            "ENN (EditedNearestNeighbours, downs. by removing samples close to the decision boundary), "                                                                                                                  
-            "upsampling (random over sampling with replacement), "
-            "SMOTE (upsampling with SMOTE - Synthetic Minority Over-sampling Technique), "
-            "SMOTETomek (over+undersampling with SMOTE+Tomek)",
+    parser.add_argument('--dim_reduc', action='store', choices=['pca'], help="optional dimensionality "
+                                                                             "reduction of input data", default=None)
+    parser.add_argument('--norms', action='store_true', help="perform normalisations? (default: True)",
+                        default=True)
+    parser.add_argument('--balance', action='store',
+                        choices=["downsampling", "Tomek", "upsampling", "SMOTE", "SMOTETomek"],
+                        help="which "
+                             "strategy to use in case of imbalanced datasets: "
+                             "downsampling (random without replacement), "
+                             "Tomek (downs. by removing Tomek links), "
+                        # "ENN (EditedNearestNeighbours, downs. by removing samples close to the decision boundary), "                                                                                                                  
+                             "upsampling (random over sampling with replacement), "
+                             "SMOTE (upsampling with SMOTE - Synthetic Minority Over-sampling Technique), "
+                             "SMOTETomek (over+undersampling with SMOTE+Tomek)",
                         default=None)
-    parser.add_argument('--class_weights', action='store_true', help="whether to use class weights in imbalanced "
-                                                                     "datasets (inversely proportional to total "
-                                                                     "class samples)",
+    parser.add_argument('--class_weights', action='store_true',
+                        help="whether to use class weights in imbalanced datasets "
+                             "(inversely proportional to total class samples)",
                         default=False
                         )
     parser.add_argument('--kernel', action='store',
-                        help="type of kernel to use (default and recommended choice is LinearSVC; possible alternatives are linear, sigmoid, rbf and poly, as per sklearn.svm.SVC)",
+                        help="type of kernel to use (default and recommended choice is LinearSVC; "
+                             "possible alternatives are linear, sigmoid, rbf and poly, as per sklearn.svm.SVC)",
                         default="LinearSVC", choices=['LinearSVC', 'linear', 'sigmoid', 'rbf', 'poly'], type=str)
-    parser.add_argument('--final', action='store_true', help="final analysis on unknown dataset (no evaluation)?", default=False)
-    parser.add_argument('--get_coefs', action='store_true', help="switch to write to disk and plots the most important coefficients for the training feats for each class",
+    parser.add_argument('--final', action='store_true',
+                        help="final analysis on unknown dataset (no evaluation)?",
+                        default=False)
+    parser.add_argument('--get_coefs', action='store_true',
+                        help="switch to write to disk and plot the most important coefficients"
+                             " for the training feats for each class",
                         default=False)
     args = parser.parse_args()
 
-    # path = "data/feats_tests_train.csv"
-    # train_path = "data/feats_tests_realForTest.csv"
-    # test_path = "data/feats_tests_valid_realForTest.csv"
     print(".......... loading data ........")
     train = pandas.read_csv(args.train_path, index_col=0)
 
@@ -54,6 +62,18 @@ if __name__ == "__main__":
 
     svm = superstyl.svm.train_svm(train, test, cross_validate=args.cross_validate, k=args.k, dim_reduc=args.dim_reduc,
                                   norms=args.norms, balance=args.balance, class_weights=args.class_weights,
-                                   kernel=args.kernel, final_pred=args.final, get_coefs=args.get_coefs)
+                                  kernel=args.kernel, final_pred=args.final, get_coefs=args.get_coefs)
 
-    joblib.dump(svm, 'mySVM.joblib')
+    if args.o is not None:
+        args.o = args.o + "_"
+
+    svm["confusion_matrix"].to_csv(args.o+"confusion_matrix.csv")
+    svm["misattributions"].to_csv(args.o+"misattributions.csv")
+    joblib.dump(svm["pipeline"], args.o+'mySVM.joblib')
+
+    print(".......... Writing final predictions to " + args.o + "FINAL_PREDICTIONS.csv ........")
+    svm["final_predictions"].to_csv(args.o+"FINAL_PREDICTIONS.csv")
+
+    if args.get_coefs:
+        print(".......... Writing coefficients to disk ........")
+        svm["coefficients"].to_csv(args.o+"coefficients.csv")
